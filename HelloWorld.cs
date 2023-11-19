4b83f0fb19b931e2f45f2cfdb105ece3cbc1a02e
EgoMrBig <151434948+EgoMrBig@users.noreply.github.com>
2023-11-19 13:17:38 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    
+    void Start()
+    {// Вывод на консоль
+        print("Hello, World!");
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
